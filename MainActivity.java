3ada17d4e46d62980a336e67f967ee6ca68c1c8b
msdhiliwal <06sakshijain2004@gmail.com>
2024-06-24 02:19:25 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,86 +1,120 @@
-package com.example.todolist;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import androidx.appcompat.app.AlertDialog;
+package com.example.maths;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTaskClickListener {
-
-    private RecyclerView recyclerView;
-    private TaskAdapter adapter;
-    private List<Task> taskList;
-    private Button addTaskButton;
-
+public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    String val,val1,val2,res,msg;
+    EditText et1,et2;
+    RadioButton sum,sub,mul,div;
+    TextView t1;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        addTaskButton = findViewById(R.id.addTaskButton);
-
-        taskList = new ArrayList<>();
-        adapter = new TaskAdapter(taskList, this);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-
-        addTaskButton.setOnClickListener(v -> showAddEditTaskDialog(null, -1));
-    }
-
-    private void showAddEditTaskDialog(Task task, int position) {
-        boolean isEdit = task != null;
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.dialog_add_edit_task, null);
-        builder.setView(dialogView);
-
-        EditText taskTitleEditText = dialogView.findViewById(R.id.taskTitleEditText);
-        Button saveTaskButton = dialogView.findViewById(R.id.saveTaskButton);
-
-        if (isEdit) {
-            taskTitleEditText.setText(task.getTitle());
-        }
-
-        AlertDialog dialog = builder.create();
-        saveTaskButton.setOnClickListener(v -> {
-            String taskTitle = taskTitleEditText.getText().toString().trim();
-            if (!taskTitle.isEmpty()) {
-                if (isEdit) {
-                    task.setTitle(taskTitle);
-                    taskList.set(position, task);
-                } else {
-                    taskList.add(new Task(taskTitle));
+        et1 = findViewById(R.id.editText1);
+        et2 = findViewById(R.id.editText2);
+        sum = findViewById(R.id.btn1);
+        sub = findViewById(R.id.btn2);
+        mul = findViewById(R.id.btn3);
+        div = findViewById(R.id.btn4);
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((et1.getText().length()>0) && (et2.getText().length() > 0))
+                {
+                    double num1 = Double.parseDouble(et1.getText().toString());
+                    double num2 = Double.parseDouble(et2.getText().toString());
+                    double result = num1 + num2;
+                    val1 = Double.toString(num1);
+                    val2 = Double.toString(num2);
+                    val = Double.toString(result);
+                    intent=new Intent(getApplicationContext(),Second.class);
+                    msg = "You have selected '+' Button";
+                    res = val1+" + "+val2+" = "+val;
+                    intent.putExtra("msg",msg);
+                    intent.putExtra("res",res);
                 }
-                adapter.notifyDataSetChanged();
-                dialog.dismiss();
-
-                Toast.makeText(MainActivity.this,taskTitle,Toast.LENGTH_LONG).show();
+                else {
+                    Toast.makeText(MainActivity.this,"Enter Required Numbers",Toast.LENGTH_LONG).show();
+                }
             }
         });
-
-        dialog.show();
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((et1.getText().length()>0) && (et2.getText().length() > 0))
+                {
+                    double num1 = Double.parseDouble(et1.getText().toString());
+                    double num2 = Double.parseDouble(et2.getText().toString());
+                    double result = num1 - num2;
+                    val1 = Double.toString(num1);
+                    val2 = Double.toString(num2);
+                    val = Double.toString(result);
+                    intent=new Intent(getApplicationContext(),Second.class);
+                    msg = "You have selected '−' Radio Button";
+                    res = val1+" − "+val2+" = "+val;
+                    intent.putExtra("msg",msg);
+                    intent.putExtra("res",res);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Enter Required Numbers",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((et1.getText().length()>0) && (et2.getText().length() > 0))
+                {
+                    double num1 = Double.parseDouble(et1.getText().toString());
+                    double num2 = Double.parseDouble(et2.getText().toString());
+                    double result = num1 * num2;
+                    val1 = Double.toString(num1);
+                    val2 = Double.toString(num2);
+                    val = Double.toString(result);
+                    intent=new Intent(getApplicationContext(),Second.class);
+                    msg = "You have selected '×' Radio Button";
+                    res = val1+" × "+val2+" = "+val;
+                    intent.putExtra("msg",msg);
+                    intent.putExtra("res",res);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Enter Required Numbers",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((et1.getText().length()>0) && (et2.getText().length() > 0))
+                {
+                    double num1 = Double.parseDouble(et1.getText().toString());
+                    double num2 = Double.parseDouble(et2.getText().toString());
+                    double result = num1 / num2;val = Double.toString(result);
+                    val1 = Double.toString(num1);
+                    val2 = Double.toString(num2);
+                    val = Double.toString(result);
+                    intent=new Intent(getApplicationContext(),Second.class);
+                    msg = "You have selected '÷' Radio Button";
+                    res = val1+" ÷ "+val2+" = "+val;
+                    intent.putExtra("msg",msg);
+                    intent.putExtra("res",res);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Enter Required Numbers",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
-
-    @Override
-    public void onEditTaskClick(Task task, int position) {
-        showAddEditTaskDialog(task, position);
-    }
-
-    @Override
-    public void onDeleteTaskClick(Task task, int position) {
-        taskList.remove(position);
-        adapter.notifyItemRemoved(position);
+    public void submit(View view){
+        startActivity(intent);
     }
 }
